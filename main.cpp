0b86fe48cceb38d005ea6b3d9266f82fe1212632
Alex Zywicki <alexander.zywicki@gmail.com>
2016-11-06 17:20:27 -0500

    added main

--- main.cpp
+++ main.cpp
@@ -9,10 +9,16 @@
 
 #include "signal.hpp"
 #include <iostream>
-
+#include <numeric>
 
 int main(int argc, const char * argv[]) {
-
+    signals::signal_t<double(std::vector<double> const&)> sig;
+    auto&& id = signals::connect(sig,[](std::vector<double> const& v){
+        return std::accumulate(v.begin(),v.end(),0);
+    });
+    std::vector<double> v = {1,2,3,4,5};
+    auto&& result = signals::result(id,signals::emit(sig,v));
+    std::cout<<result<<std::endl;
 
     return 0;
 }
